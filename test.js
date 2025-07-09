cef8ca600feab769edbc8151ca4d963fe1938935
“Pabitra969” <pkg2000969@gmail.com>
2025-07-09 20:11:14 +0530

    assignment

---
+++ test.js
@@ -0,0 +1,24 @@
+// var can be redeclared
+// data t
+var a = 10; // var is a global variable 
+var a = 20;
+
+console.log(a);
+
+//A === B to check the value along with the data type
+
+// var & function supports hoisting but let & const doesn't
+
+// javascript can change the data type dynamically
+
+console.log(a);
+var a = 10;
+
+
+
+
+// jascript treats everything as object
+
+var abc = function foo() {
+    console.log("hello");
+}
